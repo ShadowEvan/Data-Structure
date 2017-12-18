ef24940be877d4dccc859753541a4d231058e85b
Evan <975264074@qq.com>
2017-12-18 19:12:19 +0800

    BFSTraverse

---
+++ 无向图/UndirectedGraph.cpp
@@ -0,0 +1,268 @@
+// A C Program to demonstrate adjacency list representation of graphs
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+
+typedef int Status;
+typedef int QElemtype;
+typedef int boolean;
+
+boolean* visited = 0;
+
+// A structure to represent an adjacency list node
+struct AdjListNode
+{
+    int dest;
+    struct AdjListNode* next;
+};
+
+// A structure to represent an adjacency list
+struct AdjList
+{
+    struct AdjListNode *head; // pointer to head node of list
+};
+
+// A structure to represent a graph. A graph is an array of adjancency lists.
+struct Graph
+{
+    int V; // Size of array will be V(number of vertices in graph)
+    struct AdjList *array;
+};
+
+// A function to create a new adjacency list node
+struct AdjListNode* newAdjListNode(int dest)
+{
+    struct AdjListNode* newNode = (struct AdjListNode*)malloc(sizeof(struct AdjListNode));
+    newNode->dest = dest;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// A function to create a graph of V vertices
+struct Graph* createGraph(int V)
+{
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->V = V;
+
+    // Create an array of adjacency lists. Size of array will be V
+    graph->array = (struct AdjList*)malloc(V * sizeof(struct AdjList));
+
+    // Initialize each adjacency list as empty by making a head node
+    int i;
+    for(i = 0; i < V; ++i)
+        {
+            graph->array[i].head = (AdjListNode *)malloc(sizeof(AdjListNode));
+            graph->array[i].head->next = NULL;
+            graph->array[i].head->dest = i;
+        }
+    return graph;
+}
+
+// Add an edge to an undirected graph
+void addEdge(struct Graph* graph, int src, int dest)
+{
+    // Add an edge from src to dest. A new node is added to the adjacency
+    struct AdjListNode* newNode = newAdjListNode(dest);
+
+    // The node is added at the beginning
+    newNode->next = graph->array[src].head->next;
+    graph->array[src].head->next = newNode;
+
+    // Since the graph is undirected, add an edge from dest to src also
+    newNode = newAdjListNode(src);
+    newNode->next = graph->array[dest].head->next;
+    graph->array[dest].head->next = newNode;
+}
+
+// Delete an edge from an undirected graph
+void deleteEdge(struct Graph* graph, int src, int dest)
+{
+    // Delete an edge from src to dest
+    struct AdjListNode* preNode = graph->array[src].head;
+    struct AdjListNode* delNode = preNode->next;
+
+    // Search for the location of deleted edge in the adjacency list
+    while(delNode && delNode->dest != dest)
+    {
+        preNode = preNode->next;
+        delNode = preNode->next;
+    }
+    if(!delNode) 
+    {
+        printf("Not Found!");
+        return;
+    }
+
+    // perform the removal
+    preNode->next = delNode->next;
+    free(delNode);
+    delNode = NULL;
+
+    // Since the graph is undirected, delete an edge from dest to src also
+    preNode = graph->array[dest].head;
+    delNode = preNode->next;
+    while(delNode && delNode->dest !=src)
+    {
+        preNode = preNode->next;
+        delNode = preNode->next;
+    }
+    preNode->next = delNode->next;
+    free(delNode);
+    delNode = NULL;
+}
+
+// A function to print the adjacency list representation of graph
+void printGraph(struct Graph* graph)
+{
+    int v;
+    for(v = 0; v < graph->V; ++v)
+    {
+        struct AdjListNode* p = graph->array[v].head->next;
+        printf("\n Adjacency list of vertex %d\n head", v);
+        while(p)
+        {
+            printf(" -> %d", p->dest);
+            p = p->next;
+        }
+        printf("\n");
+    }
+}
+
+void DFS(struct Graph* graph, int v)
+{
+    AdjListNode* w = 0;
+
+    // mark the current node as visited and print it
+    visited[v] = TRUE;  
+    printf(" Vertex%d  ", v);
+
+    // recur for all the vertices adjacent to the vertex
+    for(w = graph->array[v].head; w != NULL; w = w->next)
+        if(visited[w->dest] == FALSE) DFS(graph, w->dest);
+}
+
+// DFS traversal of the vertices reachable from v
+// It uses recursive DFS()
+void DFSTraverse(struct Graph* graph)
+{
+    // mark all the vertices as not visited
+    int v;
+    visited = (boolean *)malloc(graph->V * sizeof(boolean));    
+    for(v = 0; v < graph->V; ++v)visited[v] = FALSE;
+
+    // call the recursive helper function
+    // and make sure all the vertices will be visited
+    for(v = 0; v < graph->V; ++v)
+        if(visited[v] == FALSE)DFS(graph, v);
+}
+
+typedef struct QNode
+{
+    QElemtype data;
+    struct QNode* next;
+}QNode, *QueuePtr;
+
+typedef struct
+{
+    QueuePtr front;
+    QueuePtr rear;
+}LinkQueue;
+
+Status InitQueue(LinkQueue &Q)
+{
+    QNode *p;
+    p = (QueuePtr)malloc(sizeof(QNode));
+    if(!p)return ERROR;
+    p->next = NULL;
+    Q.front = p;
+    Q.rear = p;
+    return OK;
+}
+
+Status EnQueue(LinkQueue &Q, QElemtype e)
+{
+    QNode *p;
+    p = (QueuePtr)malloc(sizeof(QNode));
+    if(!p)return ERROR;
+    p->data = e;
+    p->next = NULL;
+    Q.rear->next = p;
+    Q.rear = p;
+    return OK;
+}
+
+Status DeQueue(LinkQueue &Q, QElemtype &e)
+{
+    if(Q.front == Q.rear)return ERROR;
+    QNode *p;
+    p = Q.front->next;
+    e = p->data;
+    Q.front->next = p->next;
+    if(p == Q.rear)Q.rear = Q.front;
+    free(p);
+    return OK; 
+}
+
+void BFSTraverse(Graph* graph)
+{
+    int v, u;
+    AdjListNode *w = 0;
+    LinkQueue Q;
+    visited = (boolean *)malloc(graph->V * sizeof(boolean));
+    for(v = 0; v < graph->V; ++v)visited[v] = FALSE;
+    InitQueue(Q);
+    for(v = 0; v < graph->V; ++v)
+        if(visited[v] == FALSE)
+        {
+            visited[v] = TRUE;
+            printf(" Vertex%d  ", v);
+            EnQueue(Q, v);
+            while(Q.front != Q.rear)
+            {
+                DeQueue(Q, u);
+                for(w = graph->array[u].head; w != NULL; w = w->next)
+                    if(visited[w->dest] == FALSE)
+                    {
+                        visited[w->dest] = TRUE;
+                        printf(" Vertex%d  ", w->dest);
+                        EnQueue(Q, w->dest);
+                    }
+            }
+        }
+}
+
+int main(void)
+{
+    // create the graph given
+    int V = 5;
+    struct Graph* graph = createGraph(V);
+    // test addEdge();
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 4);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 1, 4);
+    addEdge(graph, 2, 3);
+    addEdge(graph, 3, 4);
+    // print the adjacency list representation of the above graph
+    printGraph(graph);
+
+    // test Traverse();
+    printf("\n The result of traversal:\n");
+    DFSTraverse(graph);
+    // BFSTraverse(graph);
+    printf("\n");
+
+    // test deleteEdge();
+    printf("\n After deleting:");
+    deleteEdge(graph, 1, 4);
+    deleteEdge(graph, 2, 3);
+    // print the adjacency list representation of the above graph
+    printGraph(graph);
+
+    return 0;
+}
